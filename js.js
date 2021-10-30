a189c004226e003929b3e6022c3f86326f5c9563
sagwamas <83221049+sagwamas@users.noreply.github.com>
2021-10-31 00:03:56 +0900

    Add files via upload

---
+++ js.js
@@ -0,0 +1,33 @@
+//"use strict"
+//const fs = require('fs');
+
+
+//const list = fs.readFile('./db/data.json', 'utf-8', (err, data) => {
+//    const rlawns = JSON.parse(data)
+//    console.log(rlawns.questions[0].question);
+
+//})
+//import data from ".data.json";
+
+//console.log(data);
+
+
+// /**
+//  * 저장 버튼 클릭 함수
+//  */
+  function handleSave() {}
+
+//  /**
+//   * 오른쪽 화살표 버튼 클릭 함수
+//   */
+ function handleNext() {}
+//  /**
+//   * 왼쪽 화살표 버튼 클릭 함수
+//   */
+  function handlePrev() {}
+
+//   * 서비스 초기화 함수
+   
+  function init() {}
+ 
+//  init();
